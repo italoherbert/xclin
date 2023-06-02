c0782f98dd59975d573570d4d58f4eb46f30305f
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2023-06-02 16:56:17 -0300

    Após ajustado o feign com verificação de SSL desabilitada

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Xclin - Sistema de gerenciamento de clínicas';
 }
---
+++ src/main/java/italo/xclin/config/FeighConfiguration.java
@@ -0,0 +1,8 @@
+package italo.xclin.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeighConfiguration {  
+		
+}

4cf39d53a0bae9a91b0044b20f84baf768fa0a65
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2023-07-08 12:38:37 -0300

    Alterado de Dispesa para Despesa

--- src/main/java/italo/xclin/enums/tipos/LancamentoTipo.java
+++ src/main/java/italo/xclin/enums/tipos/LancamentoTipo.java
@@ -6,7 +6,7 @@ public enum LancamentoTipo {
 	public String label() {
 		switch( this ) {
 			case CREDITO: return "Receita";
-			case DEBITO: return "Dispesa";
+			case DEBITO: return "Despesa";
 		}
 		return null;
 	}
